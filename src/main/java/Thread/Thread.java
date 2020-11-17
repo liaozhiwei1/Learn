0af5e0ef82936a4dfee7fd53119d6327eb2de1a8
liaozhiwei1 <1163230808@qq.com>
2020-11-17 16:00:42 +0800

    1

---
+++ src/main/java/Thread/Thread.java
@@ -0,0 +1,49 @@
+package Thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @version: 1.0
+ * @description:
+ * @author: zhiwei.liao
+ * @create: 2020-11-17 10:00:56
+ **/
+//1
+class Thread1 implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
+//2
+class  Thread2 implements Callable{
+
+    @Override
+    public String call() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("正在计算");
+            java.lang.Thread.sleep(1000);
+        }
+        return 1024+"";
+    }
+}
+//3 class Thread3 extends Thread
+
+//4 线程池
+
+
+public class Thread{
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Thread2 thread2 = new Thread2();
+        FutureTask<String> futureTask = new FutureTask(thread2);
+        new java.lang.Thread(futureTask,"aaa").start();
+        for (int i=0;i<10;i++) {
+            java.lang.Thread.sleep(500);
+            System.out.println("1111");
+        }
+        System.out.println(futureTask.get());
+    }
+}
