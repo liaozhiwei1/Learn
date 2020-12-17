f5351a5f71c3d1ceb2677793d4e774138458290c
liaozhiwei1 <1163230808@qq.com>
2020-12-17 09:51:44 +0800

    1

---
+++ src/main/java/Spring/Service/LiaoBeanFactoryPostProcess.java
@@ -0,0 +1,22 @@
+package Spring.Service;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @version: 1.0
+ * @description:
+ * @author: zhiwei.liao
+ * @create: 2020-12-08 20:54:10
+ **/
+@Configuration
+public class LiaoBeanFactoryPostProcess implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        BeanDefinition serviceA = beanFactory.getBeanDefinition("serviceA");
+//        serviceA.setScope("SCOPE_SINGLETON");
+    }
+}
