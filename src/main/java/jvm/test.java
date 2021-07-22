c2f27090ed07d07beab654b371b859fadfb5f4df
zhiwei.liao1@baozun.com <123456789>
2021-07-22 15:27:41 +0800

    1

---
+++ src/main/java/jvm/test.java
@@ -0,0 +1,57 @@
+package jvm;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * All rights Reserved, Designed By www.baozun.com
+ *
+ * @title: test
+ * @package: jvm
+ * @description: (用一句话描述该文件做什么)
+ * @author: xugejun(giorno.xu @ baozun.com)
+ * @date: 2021/7/21 10:48 上午
+ * @version: v1.0
+ * @copyright: 2021 www.baozun.com Inc. All rights reserved.
+ * 注意：本内容仅限于上海宝尊电商内部传阅，禁止外泄以及用于其他的商业目的
+ */
+public class test {
+    private Lock lock =new ReentrantLock();
+    private Lock lock1 = new ReentrantLock();
+    public static  void  main(String[] args) throws InterruptedException {
+        List list = new LinkedList();
+        for (; ; ) {
+            byte[] bytes = new byte[1024];
+            Thread.sleep(200);
+            list.add(bytes);
+        }
+//        test test = new test();
+//        new Thread(()->{
+//            try {
+//                test.t();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+//        new Thread(()->{
+//            try {
+//                test.t1();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+    }
+
+    public void t() throws InterruptedException {
+        lock.lock();
+        while (true){
+
+        }
+    }
+
+    public void t1() throws InterruptedException {
+        lock.lock();
+    }
+}
